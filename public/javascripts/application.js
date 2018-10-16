a4ecb61ad9022f3d2e0b883041e9b36df9d4796d
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2018-10-16 10:59:46 +0100

    added inital commit

---
+++ public/javascripts/application.js
@@ -0,0 +1,75 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init()
+
+  // Details/summary polyfill from frontend toolkit
+  GOVUK.details.init()
+
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+
+
+ $.getJSON('https://www.bathnes.gov.uk/WebAPI/api/AddressesAPI/v2/search/bs312tr/0/false', function(data) {
+ console.log(data);
+});
+
+var person = {
+id:2343,
+firstName:"John",
+lastName:"Doe",
+age:46,
+telephone:"07702 264922",
+stage:1,
+documents: "4 out of 5 documents collected",
+message: "Richard Rocks"
+};
+
+console.log(person);
+
+$('a#myBtn').click(function(e) {
+    e.preventDefault();
+    //return false;
+});
+
+
+})
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
